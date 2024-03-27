57b3c3ca36174b439024ed3aca071577cd4f3520
AshBorn <anishsingh0119@gmail.com>
2024-03-27 16:01:30 +0530

    Change url

--- AnieXEricaMusic/plugins/bot/start.py
+++ AnieXEricaMusic/plugins/bot/start.py
@@ -27,7 +27,7 @@ from strings import get_string
 
 # Define a list of video URLs
 video_urls = [
-    "https://graph.org/file/8f5299f1d37a3ae17ecbc.mp4",
+    "https://graph.org/file/937e10d39716e624f4b1c.mp4",
 ]
 
 # Define a list of animated emojis
